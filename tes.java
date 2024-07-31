a705c898ed157785212607446ae373be08e50257
Althaf-Arya <althaf_aryasatya_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:14:03 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,7 @@
+public class Tes {
+    public static void main(String[] args) {
+        String nama = "Althaf";
+        String kelas ="X RPL 5";
+        System.out.print("Halo, nama saya " + Althaf " dari kelas " + X RPL 5);
+    };
+}
